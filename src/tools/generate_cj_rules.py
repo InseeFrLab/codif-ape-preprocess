cf78d7d5c13a7e7345aa1461b3759ce164b44ab2
TheAIWizard <nathanpanoel@hotmail.fr>
2025-08-23 03:23:18 +0000

    fix rules names and add threshold, cj rev2

--- src/tools/generate_cj_rules.py
+++ src/tools/generate_cj_rules.py
@@ -12,7 +12,7 @@ from utils.io import download_json
 CJ_JSON_PATH = "s3://projet-ape/data/cj.json"
 OUTPUT_DIR = "./rules/naf_2025"
 
-TEMPLATE = """\
+TEMPLATE_2025 = """\
 import pandas as pd
 from core.decorators import rule, track_new
 
@@ -32,6 +32,26 @@ def add_cj_{code}_modality_2025(df: pd.DataFrame,methods=None, methods_params=No
     return pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
 """
 
+TEMPLATE_REV2 = """\
+import pandas as pd
+from core.decorators import rule, track_new
+
+@rule(
+    name="cj_{code}_record_rev2",
+    tags=["naf_rev2"],
+    description="Ensure presence of CJ modality {code} (NAF rev 2)",
+)
+@track_new(column='apet_finale')
+def add_cj_{code}_modality_rev2(df: pd.DataFrame,methods=None, methods_params=None) -> pd.DataFrame:
+    new_row = {{
+        "liasse_numero": "J00addCJ{code}",
+        "cj": "{code}",
+        "libelle": "LMNP",
+        "nace2025": "6820A",
+    }}
+    return pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
+"""
+
 
 def main():
     # Load JSON into a DataFrame
@@ -41,12 +61,21 @@ def main():
     os.makedirs(OUTPUT_DIR, exist_ok=True)
 
     for code in df_codes["code"].dropna():
-        file_name = f"ensure_cj_{code}_present.py"
+        file_name = f"ensure_cj_{code}_present_2025.py"
         file_path = os.path.join(OUTPUT_DIR, file_name)
-        content = TEMPLATE.format(code=code)
+        content_2025 = TEMPLATE_2025.format(code=code)
 
         with open(file_path, "w", encoding="utf-8") as f:
-            f.write(content)
+            f.write(content_2025)
+
+        print(f"✅ Generated {file_path}")
+
+        file_name = f"ensure_cj_{code}_present_rev2.py"
+        file_path = os.path.join(OUTPUT_DIR, file_name)
+        content_rev2 = TEMPLATE_REV2.format(code=code)
+
+        with open(file_path, "w", encoding="utf-8") as f:
+            f.write(content_rev2)
 
         print(f"✅ Generated {file_path}")
 
