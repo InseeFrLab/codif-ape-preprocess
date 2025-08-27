94f15a9bfb1458fc247958ad518fda5ca13602b4
TheAIWizard <nathanpanoel@hotmail.fr>
2025-08-27 14:02:22 +0000

    fix default input rev path

--- src/constants/paths.py
+++ src/constants/paths.py
@@ -1,10 +1,10 @@
 PREFIX = "s3://projet-ape/data/"
-FOLDER = "25032024_26082025"
+FOLDER = "25032024_26082025/"
 
-URL_SIRENE4_NAFREV2 = PREFIX + FOLDER + "nafrev2" + "raw.parquet"
-URL_SIRENE4_NAF2025 = PREFIX + FOLDER + "naf2025" + "raw.parquet"
+URL_SIRENE4_NAFREV2 = PREFIX + FOLDER + "nafrev2/" + "raw.parquet"
+URL_SIRENE4_NAF2025 = PREFIX + FOLDER + "naf2025/" + "raw.parquet"
 
-URL_OUTPUT_NAF2025 = PREFIX + FOLDER + "naf2025" + "raw_cleansed.parquet"
-URL_REPORT_NAF2025 = PREFIX + FOLDER + "naf2025" + "raw_cleansed_report.parquet"
-URL_OUTPUT_NAFREV2 = PREFIX + FOLDER + "nafrev2" + "raw_cleansed.parquet"
-URL_REPORT_NAFREV2 = PREFIX + FOLDER + "nafrev2" + "raw_cleansed_report.parquet"
+URL_OUTPUT_NAF2025 = PREFIX + FOLDER + "naf2025/" + "raw_cleansed.parquet"
+URL_REPORT_NAF2025 = PREFIX + FOLDER + "naf2025/" + "raw_cleansed_report.parquet"
+URL_OUTPUT_NAFREV2 = PREFIX + FOLDER + "nafrev2/" + "raw_cleansed.parquet"
+URL_REPORT_NAFREV2 = PREFIX + FOLDER + "nafrev2/" + "raw_cleansed_report.parquet"
