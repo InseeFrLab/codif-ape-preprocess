dd74d5cc88ea73e8e416f395301327f3f30fe1a8
meilame-tayebjee <meilame.tayebjee@polytechnique.edu>
2025-09-01 15:39:07 +0000

    commit: chore: update paths for saving

--- src/constants/paths.py
+++ src/constants/paths.py
@@ -1,10 +1,15 @@
 PREFIX = "s3://projet-ape/data/"
 FOLDER = "25032024_26082025/"
+ARTIFACTS_FOLDER = "artifacts/data_cleaning/"
 
-URL_SIRENE4_NAFREV2 = PREFIX + FOLDER + "nafrev2/" + "raw.parquet"
+URL_SIRENE4_NAFREV2 = PREFIX + FOLDER + "naf2008/" + "raw.parquet"
 URL_SIRENE4_NAF2025 = PREFIX + FOLDER + "naf2025/" + "raw.parquet"
 
 URL_OUTPUT_NAF2025 = PREFIX + FOLDER + "naf2025/" + "raw_cleansed.parquet"
-URL_REPORT_NAF2025 = PREFIX + FOLDER + "naf2025/" + "raw_cleansed_report.parquet"
-URL_OUTPUT_NAFREV2 = PREFIX + FOLDER + "nafrev2/" + "raw_cleansed.parquet"
-URL_REPORT_NAFREV2 = PREFIX + FOLDER + "nafrev2/" + "raw_cleansed_report.parquet"
+URL_REPORT_NAF2025 = (
+    PREFIX + FOLDER + "naf2025/" + ARTIFACTS_FOLDER + "raw_cleansed_report.parquet"
+)
+URL_OUTPUT_NAFREV2 = PREFIX + FOLDER + "naf2008/" + "raw_cleansed.parquet"
+URL_REPORT_NAFREV2 = (
+    PREFIX + FOLDER + "naf2008/" + ARTIFACTS_FOLDER + "raw_cleansed_report.parquet"
+)
