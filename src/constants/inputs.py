02d041a6e47cc717ed6e49b9d903a2e140fa31d1
Nathan Randriamanana <37664429+TheAIWizard@users.noreply.github.com>
2025-09-02 11:03:27 +0200

    fix columns to delete: _clean --> _cleaned

--- src/constants/inputs.py
+++ src/constants/inputs.py
@@ -1,2 +1,3 @@
 TEXTUAL_INPUTS = ["libelle", "activ_sec_agri_et", "activ_nat_lib_et"]
-TEXTUAL_INPUTS_CLEANED = [col + "_clean" for col in TEXTUAL_INPUTS]
+TEXTUAL_INPUTS_CLEANED = [col + "_cleaned" for col in TEXTUAL_INPUTS]
+# if less cleaning needed (ex: attention model), comment/delete the previous line or rename cleaned columns 
