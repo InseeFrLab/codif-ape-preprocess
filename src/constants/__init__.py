b99f6a03cd3fd87405d675e455738d513d994a88
meilame-tayebjee <meilame.tayebjee@polytechnique.edu>
2025-09-02 15:53:45 +0000

    feat: add preprocessing constants

---
+++ src/constants/__init__.py
@@ -0,0 +1,36 @@
+from .col_renaming import (
+    CATEGORICAL_FEATURES as CATEGORICAL_FEATURES,
+)
+from .col_renaming import (
+    COL_RENAMING as COL_RENAMING,
+)
+from .col_renaming import (
+    SURFACE_COLS as SURFACE_COLS,
+)
+from .col_renaming import (
+    TEXT_FEATURE as TEXT_FEATURE,
+)
+from .col_renaming import (
+    TEXTUAL_FEATURES as TEXTUAL_FEATURES,
+)
+from .paths import (
+    URL_DF_NAF2008 as URL_DF_NAF2008,
+)
+from .paths import (
+    URL_DF_NAF2025 as URL_DF_NAF2025,
+)
+from .paths import (
+    URL_MAPPINGS as URL_MAPPINGS,
+)
+from .paths import (
+    URL_OUTPUT_NAF2025 as URL_OUTPUT_NAF2025,
+)
+from .paths import (
+    URL_OUTPUT_NAFREV2 as URL_OUTPUT_NAFREV2,
+)
+from .targets import (
+    NACE_REV2_1_COLUMN as NACE_REV2_1_COLUMN,
+)
+from .targets import (
+    NACE_REV2_COLUMN as NACE_REV2_COLUMN,
+)
