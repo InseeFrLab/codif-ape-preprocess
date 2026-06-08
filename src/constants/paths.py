ecfb84db9001f97400b751e4da9c3ff945ac1aa8
TheAIWizard <nathanpanoel@hotmail.fr>
2026-06-08 10:02:36 +0000

    feat: next ds

--- src/constants/paths.py
+++ src/constants/paths.py
@@ -1,5 +1,5 @@
 PREFIX = "s3://projet-ape/data/"
-FOLDER = "03062026/"
+FOLDER = "08062026/"
 ARTIFACTS_FOLDER = "artifacts/data_cleaning/"
 
 URL_SIRENE4_NAFREV2 = PREFIX + FOLDER + "naf2008/" + "raw.parquet"
